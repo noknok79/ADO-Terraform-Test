fa76053c9211fb27f230b7424b11fbd310153794
Mark Villanueva <nokiev@yahoo.com>
2024-01-18 20:58:37 +0800

    Partsunlimited Deployment

--- terraform-manifests/partsunlimited-node.tf
+++ terraform-manifests/partsunlimited-node.tf
@@ -1,11 +1,11 @@
-resource "azurerm_kubernetes_cluster_node_pool" "linux-partsunlimited" {
+resource "azurerm_kubernetes_cluster_node_pool" "linuxpartsunlimited" {
   zones                 = [1, 2, 3]
   enable_auto_scaling   = true
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
   max_count             = 3
   min_count             = 1
   mode                  = "User"
-  name                  = "linux-partsunlimited"
+  name                  = "linuxpartsunlimited"
   orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   os_disk_size_gb       = 30
   os_type               = "Linux" # Default is Linux, we can change to Windows
